653cce0b97afd75288bbb786253bef6151e45fa4
AleenaAsif <aleenaasif.444@gmail.com>
2023-04-16 16:23:19 +0500

    updated java code

--- app/src/main/java/com/myproject/fuelcost_calculator_andriod/MainActivity.java
+++ app/src/main/java/com/myproject/fuelcost_calculator_andriod/MainActivity.java
@@ -1,14 +1,28 @@
 package com.myproject.fuelcost_calculator_andriod;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare the EditText and TextView widgets that are defined in the XML layout file.
+    EditText distanceInput, efficiencyInput, fuelcostInput;
+    TextView resultOutput;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        distanceInput=findViewById(R.id.editTextNumberDecimal);
+        efficiencyInput=findViewById(R.id.editTextNumberDecimal2);
+        fuelcostInput=findViewById(R.id.editTextNumberDecimal3);
     }
+
+    
 }
