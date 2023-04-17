b42c9ba43135d8d7b232d6dd722df605d832f9a8
AleenaAsif <aleenaasif.444@gmail.com>
2023-04-17 20:26:03 +0500

    update (java & activity main)
    implemented null checks, icon updated.

--- app/src/main/java/com/myproject/fuelcost_calculator_andriod/MainActivity.java
+++ app/src/main/java/com/myproject/fuelcost_calculator_andriod/MainActivity.java
@@ -5,6 +5,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,27 +27,37 @@ public class MainActivity extends AppCompatActivity {
         resultText=findViewById(R.id.textView4);
     }
 
-    public void calculateFuelCost(View view){
+    public void calculateFuelCost(View view) {
 
         //View parameter represents the button view that was clicked.
 
         //input from get text comes as char, we convert it to string to parse it to Double
+        if (distanceInput.getText().toString().isEmpty() || efficiencyInput.getText().toString().isEmpty() || fuelcostInput.getText().toString().isEmpty()) {
 
+            Toast.makeText(this, "Please enter values in all fields", Toast.LENGTH_LONG).show();
+            return;
+            //Toast class object , set duration for popup message
+        }
+
+        else{
         double distance = Double.parseDouble(distanceInput.getText().toString());
-        double efficiency=Double.parseDouble(efficiencyInput.getText().toString());
+        double efficiency = Double.parseDouble(efficiencyInput.getText().toString());
         double fuelCost = Double.parseDouble(fuelcostInput.getText().toString());
+
+        //check for null values
+
 
         //formula for calculating fuel cost
 
-        double totalCost = (distance / efficiency) * fuelCost;
+            double totalCost = (distance / efficiency) * fuelCost;
 
-        //format fuelCost value as a string with two decimal places using the String.format() method.
-        // The "%.2f" tells the method to format the double value with two decimal places.
+            //format fuelCost value as a string with two decimal places using the String.format() method.
+            // The "%.2f" tells the method to format the double value with two decimal places.
 
-        String result=  String.format("%.2f", totalCost);
-        resultOutput.setText(result);
-        resultText.setVisibility(View.VISIBLE);
-        resultOutput.setVisibility(View.VISIBLE);
+            String result = String.format("%.2f", totalCost);
+            resultOutput.setText(result);
+            resultText.setVisibility(View.VISIBLE);
+            resultOutput.setVisibility(View.VISIBLE);
+        }
     }
-
 }
